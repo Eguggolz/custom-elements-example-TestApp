d63ae07b7571d83b09cae3a97622fdeda60c33e0
ellen.guggolz <ellen.guggolz@vinci-energies.net>
2019-10-16 14:47:17 +0200

    Example TestApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  loaded:boolean =false;
+  constructor(){
+    this.loadScript();
+  }
+  public loadScript(): void{
+    if(this.loaded){
+      return;
+    }else{
+      const script = document.createElement('script');
+      script.src ="assets/custom-elements.js";
+      document.body.appendChild(script);
+      this.loaded = true;
+    }
+  }
+
 }
